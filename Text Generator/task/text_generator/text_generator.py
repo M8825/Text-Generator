bf24fbdedd345f6d79508b695d307c72739aeb27
MkMula <maho.mamulashvili@gmail.com>
2022-03-18 17:03:04 -0400

    Generate full sentence
    Generates pseudo-sentences based on number bigrma word occurrences in text.
    Prints 10 lines. Line starts with capital letter and ends with <.?!>.
    length of pseudo-sentences are at least 5 words.

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,35 +1,46 @@
 from nltk.tokenize import WhitespaceTokenizer
-from nltk.probability import FreqDist
 from nltk import bigrams
 from collections import Counter
+from random import choice, choices
+import re
 
 file = open(input(), 'r', encoding='utf-8').read()
 
 corpus = WhitespaceTokenizer().tokenize(file)
 bgrms = list(bigrams(corpus))
-total_bigrams = FreqDist(bgrms).N()
 
-print(f'Number of bigrams: {total_bigrams}')
 
-while True:
-    usr_inp = input()
+# Create dictionary of unique head and all available keys.
+chain = {}
+for head, tail in bgrms:
+    chain.setdefault(head, []).append(tail)
 
-    if usr_inp == 'exit': break
-    else:
-        try:
-            # Create dictionary of unique head and all available keys.
-            chain = {}
-            for head, tail in bgrms:
-                chain.setdefault(head, []).append(tail)
-            tails = Counter(chain[usr_inp]).most_common()  # TODO Maybe check usr_inp type?
-            print('Head:', usr_inp)
+for _ in range(10):
+    new_line = []
+    c = 0
+    while True:
+        if c == 0:  # When sentence starts with lower case
+            head_word = choice(corpus)
+            is_start = bool(re.match(r'^[A-Z].*[^.?!]$', head_word) )
+            if not is_start: continue
+            else:
+                new_line.append(head_word)
 
-            for word, count in tails:
-                print('Tail:', word, ' Count:', count)
+        tails = Counter(chain[head_word]).most_common()
 
-        except (ValueError, TypeError):
-            print('Type Error. Please input an integer.')
-        except IndexError:
-            print('Index Error. Please input an integer that is in the range of the corpus.')
-        except KeyError:
-           print('Key Error. The requested word is not in the model. Please input another word.')
+        # Create list of Tails and Counts
+        tails_lst, weights_lst = [], []
+        for word, count in tails:
+            tails_lst.append(word)
+            weights_lst.append(count)
+        next_word = ''.join(choices(tails_lst, weights=weights_lst))  # choices() returns [] we need str()
+
+        head_word = next_word  # New head for next tails
+        new_line.append(next_word)
+
+        if c >= 3:
+            is_end = bool(re.match(r'.*[.?!]$', head_word))
+            if is_end: break
+        c += 1
+
+    print(*new_line, sep=' ', end='\n')
