506db4bb08b5b28c2f019cbb4b338f410c350e48
MkMula <maho.mamulashvili@gmail.com>
2022-03-21 14:52:38 -0400

    Trigrams instead of Bigrams
    Transform the list of Bigrams into a list of Trigrams. <head> consists
    of two space-separated tokens concatenated into a single string.
    Beginning of the chain is a randomly chosen <head> from the model.
    
    Generated pseudo-sentences make a little more sense than in previous
    stages

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,28 +1,27 @@
 from nltk.tokenize import WhitespaceTokenizer
-from nltk import bigrams
+from nltk import trigrams
 from collections import Counter
 from random import choice, choices
 import re
 
 file = open(input(), 'r', encoding='utf-8').read()
 
-corpus = WhitespaceTokenizer().tokenize(file)
-bgrms = list(bigrams(corpus))
+corpora = WhitespaceTokenizer().tokenize(file)  # Create corpora based of spaces between words
+tri_grams = list(trigrams(corpora))
 
-
-# Create dictionary of unique head and all available keys.
+# Create dictionary of unique 2 word <head> and 1 word <tail>.
 chain = {}
-for head, tail in bgrms:
-    chain.setdefault(head, []).append(tail)
+for head_one, head_two, tail in tri_grams:
+    chain.setdefault(f'{head_one} {head_two}', []).append(tail)
 
 for _ in range(10):
     new_line = []
     c = 0
     while True:
-        if c == 0:  # When sentence starts with lower case
-            head_word = choice(corpus)
-            is_start = bool(re.match(r'^[A-Z].*[^.?!]$', head_word) )
-            if not is_start: continue
+        if c == 0:  # Find proper start word for sentence
+            head_word = choice(list(chain.keys()))
+            is_start = bool(re.match(r'^[A-Z].*[^.?!]\s', head_word))  # Check if word matches beginning of the sentence
+            if not is_start: continue                                  # criteria
             else:
                 new_line.append(head_word)
 
@@ -35,12 +34,12 @@ for _ in range(10):
             weights_lst.append(count)
         next_word = ''.join(choices(tails_lst, weights=weights_lst))  # choices() returns [] we need str()
 
-        head_word = next_word  # New head for next tails
         new_line.append(next_word)
+        head_word = ' '.join(' '.join(new_line).split()[-2:])  # New <head> consists last 2 words in developing line
 
-        if c >= 3:
-            is_end = bool(re.match(r'.*[.?!]$', head_word))
+        if c >= 2:
+            is_end = bool(re.match(r'.*[.?!]$', head_word))  # Check if word matches end of the sentence criteria
             if is_end: break
         c += 1
 
-    print(*new_line, sep=' ', end='\n')
+    print(*new_line, sep=' ', end='\n')  # Print each line.
